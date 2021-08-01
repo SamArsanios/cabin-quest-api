c0daf74d59666cd9044c0825198facda69fd7f4b
SamArsanios <samson.kibrom@hotmail.com>
2021-08-01 08:53:27 +0300

    Modify methods to make them return json

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,6 +8,7 @@ module Api
       # GET /users.json
       def index
         @users = User.all
+        render json: @users
       end
 
       # GET /users/1
@@ -25,7 +26,7 @@ module Api
         @user = User.new(user_params)
 
         if @user.save
-          render :show#, status: :created
+          render json: @user#, status: :created
         else
           render json: @user.errors#, status: :unprocessable_entity
         end
@@ -36,7 +37,7 @@ module Api
       def update
         if current__user.isAdmin || current__user == @user
           if @user.update(user_params)
-            render :show#, status: :ok
+            render json: @user#, status: :ok
           else
             render json: @user.errors#, status: :unprocessable_entity
           end
