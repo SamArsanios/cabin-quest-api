028bdad0a7b8000470a45eb92a2934aaab1bfb8f
SamArsanios <samson.kibrom@hotmail.com>
2021-08-07 00:20:20 +0300

    update cors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,7 +42,10 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # config.hosts << ENV['NGROK_HOST'] if ENV['NGROK_HOST'].present?
+  # export NGROK_HOST=15e1a6b21b4c.ngrok.io
 
+  config.hosts << "5712317d944e.ngrok.io"
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
