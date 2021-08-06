8abdca93c21581423458cd75a0f0697d3ce25595
SamArsanios <samson.kibrom@hotmail.com>
2021-08-07 00:19:00 +0300

    Add cloudinary keys

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+    config.cloud_name = 'dxc1u5evw'
+    config.api_key = '677957218387247'
+    config.api_secret = '1V136ij22eN8rgIHjUvlxgfz8C8'
+    config.secure = true
+    config.cdn_subdomain = true
+end
