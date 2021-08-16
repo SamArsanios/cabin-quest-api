50427b78572f76c6714b96a8d7de61dcbcdb838f
SamArsanios <samson.kibrom@hotmail.com>
2021-08-16 22:06:55 +0300

    Add uniqueness to users in db schema

---
+++ db/migrate/20210816190413_add_uniqueness_to_users.rb
@@ -0,0 +1,6 @@
+class AddUniquenessToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_index :users, :username, unique: true
+    add_index :users, :email, unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_122702) do
+ActiveRecord::Schema.define(version: 2021_08_16_190413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2021_07_30_122702) do
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "cabins", "users"
