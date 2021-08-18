57b957cee45fabae66ce9be31c492e6a013ade29
SamArsanios <samson.kibrom@hotmail.com>
2021-08-18 18:02:13 +0300

    Move  methods from user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
+
+  def find_user
+    @user = User.find_by!(username: params[:username])
+    if @user
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username,
+                                 :email, :password,
+                                 :password_confirmation,
+                                 :firstname,
+                                 :lastname)
+  end
 end
