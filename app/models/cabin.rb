a346b408e863dfb40da74d59472a13dbb13bb727
SamArsanios <samson.kibrom@hotmail.com>
2021-08-18 12:51:27 +0300

    modify cabin model slugify remmpved

--- app/models/cabin.rb
+++ app/models/cabin.rb
@@ -1,14 +1,9 @@
 class Cabin < ApplicationRecord
   belongs_to :user
-  before_save :slugify
   has_many :favourites, dependent: :destroy
 
   validates :name, uniqueness: true, presence: true, length: { in: 3..100 }
   validates_presence_of :country, :address, :region, :location
 
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
-
-  def slugify
-    self.slug = name.parameterize
-  end
 end
