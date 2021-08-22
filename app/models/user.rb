268a96faa9c21599fa8985d86d8f55ecbd1098a7
SamArsanios <samson.kibrom@hotmail.com>
2021-08-22 21:08:19 +0300

    Add users has many cabins validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :favourites, dependent: :destroy
+  has_many :cabins, dependent: :destroy
 
   validates :username, uniqueness: true, presence: true, length: { in: 3..100 }
   validates :firstname, presence: true, length: { in: 2...100 }
@@ -9,13 +10,13 @@ class User < ApplicationRecord
 
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 
-  def user_favs(user, cabins)
-    favs = []
-    cabins.each do |cabin|
-      user.favourites.each do |fav|
-        favs.push(cabin) if cabin.id == fav.cabin_id
-      end
-    end
-    favs
-  end
+  # def user_favs(user, cabins)
+  #   favs = []
+  #   cabins.each do |cabin|
+  #     user.favourites.each do |fav|
+  #       favs.push(cabin) if cabin.id == fav.cabin_id
+  #     end
+  #   end
+  #   favs
+  # end
 end
