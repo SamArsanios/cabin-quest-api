e679ac2e6ff91da62baebb095862f98e28ae23e6
SamArsanios <samson.kibrom@hotmail.com>
2021-08-22 21:06:08 +0300

    reduce complexity of routes by using resources,remove finduser and userfavourites

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do 
-        get '/user/:username', to: 'users#find_user'
-        get '/user/:id/favourites', to: 'users#user_favourites'
-        resources :users
+        # get '/user/:username', to: 'users#find_user'
+        # get '/user/:id/favourites', to: 'users#user_favourites'
+        resources :users, only: [:create, :index]
         resources :cabins
         resources :image_uploaders, only: [:create]
         resources :favourites, only: [:destroy, :create, :index]
