7cb4d0b639180e67ddda9a6df4fcde78a558b4da
SamArsanios <samson.kibrom@hotmail.com>
2021-08-23 09:48:57 +0300

    Modify index action to show cabins of only loggedin/current user

--- app/controllers/api/v1/favourites_controller.rb
+++ app/controllers/api/v1/favourites_controller.rb
@@ -4,12 +4,12 @@ module Api
       before_action :authenticate_user
 
       def index
-        @favourites = Favourite.all
+        @favourites = user_fa
         render json: @favourites
       end
 
       def create
-        favourite = user.favourites.new(favourites_params)
+        favourite = current_user.favourites.build(favourites_params)
         if favourite.save
           render json: favourite, status: :created
         else
@@ -30,9 +30,15 @@ module Api
 
       private
 
-      def user
-        @user ||= User.find(favourites_params[:user_id])
-      end
+      def user_fa 
+        cabin_ids = current_user.favourites.pluck(:cabin_id) 
+        fa_cabins = current_user.cabins.where(id: cabin_ids) 
+        fa_cabins.ordered_by_most_recent
+      end 
+
+      # def user
+      #   @user ||= User.find(favourites_params[:user_id])
+      # end
 
       def favourites_params
         params.require(:favourite).permit(:user_id, :cabin_id)
