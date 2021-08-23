7d96ad6aba74973f0c846e4301c3a98a33d57003
SamArsanios <samson.kibrom@hotmail.com>
2021-08-23 17:49:21 +0300

    Modify route usertoken for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do 
-        # get '/user/:username', to: 'users#find_user'
-        # get '/user/:id/favourites', to: 'users#user_favourites'
         resources :users, only: [:create, :index]
         resources :cabins
         resources :image_uploaders, only: [:create]
         resources :favourites, only: [:destroy, :create, :index]
-        post '/auth/signin', to: 'user_token#create'
+        resources :user_token, only: [:create]
     end
   end
 end
